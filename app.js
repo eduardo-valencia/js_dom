59dede1a31cc805afde6a6edeec4d4aa6dcd4a12
Eduardo Valencia <8025918@gmail.com>
2018-09-15 09:55:51 -0700

    Add hide list button.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const purpleThings = document.getElementsByTagName('li');
 const changeHeadingButton = document.querySelector(".change-heading");
 const heading = document.querySelector('h1');
 const changeHeadingInput = document.querySelector("#change-heading-input");
+const toggleListBtn = document.querySelector('#toggle-list');
+const list = document.querySelector("ul")
 
 for (let i=0; i < purpleThings.length; i++) {
     purpleThings[i].style.color = 'purple'
@@ -10,5 +12,16 @@ for (let i=0; i < purpleThings.length; i++) {
 changeHeadingButton.addEventListener('click', () => {
     if (changeHeadingInput.value != "") {
         heading.textContent = changeHeadingInput.value
+    }
+});
+
+toggleListBtn.addEventListener('click', () => {
+    if (list.style.display == 'none') {
+        toggleListBtn.textContent = 'Hide List';
+        list.style.display = 'block';
+    }
+    else {
+        toggleListBtn.textContent = 'Show List';
+        list.style.display = 'none';
     }
 });
