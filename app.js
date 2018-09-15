c3d46cda94e1fae960f3b98c1628e202b5010746
Eduardo Valencia <8025918@gmail.com>
2018-09-15 10:57:50 -0700

    Add a button and a text input for adding a new element to the list.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const heading = document.querySelector('h1');
 const changeHeadingInput = document.querySelector("#change-heading-input");
 const toggleListBtn = document.querySelector('#toggle-list');
 const list = document.querySelector("ul")
+const addItemInput = document.querySelector("#addItemInput")
+const addItemBtn = document.querySelector("#addItemButton")
 
 for (let i=0; i < purpleThings.length; i++) {
     purpleThings[i].style.color = 'purple'
@@ -12,6 +14,7 @@ for (let i=0; i < purpleThings.length; i++) {
 changeHeadingButton.addEventListener('click', () => {
     if (changeHeadingInput.value != "") {
         heading.textContent = changeHeadingInput.value
+        changeHeadingInput.value = '';
     }
 });
 
@@ -24,4 +27,12 @@ toggleListBtn.addEventListener('click', () => {
         toggleListBtn.textContent = 'Show List';
         list.style.display = 'none';
     }
+});
+
+addItemBtn.addEventListener('click', () => {
+    let li = document.createElement('li');
+    let ul = document.querySelectorAll('ul')[0];
+    li.textContent = addItemInput.value;
+    ul.appendChild(li);
+    addItemInput.value = '';
 });
