03254b92af605b57cdb43b71624e72d76e563a8e
Eduardo Valencia <8025918@gmail.com>
2018-09-14 18:18:02 -0700

    Added things that were not purple things. Those things now have the color red.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const purpleThings = document.getElementsByTagName('li');
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
 
 for (let i=0; i < purpleThings.length; i++) {
     purpleThings[i].style.color = 'purple'
 };
+
+for (let index = 0; index < errorNotPurple.length; index++) {
+    errorNotPurple[index].style.color = 'red';
+    
+}
